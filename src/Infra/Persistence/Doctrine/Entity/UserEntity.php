ee9cf944a2afd88361f7aae5da67533a0df9fefd
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-16 22:54:20 -0400

    fix: rename class from User to UserEntity

--- src/Infra/Persistence/Doctrine/Entity/UserEntity.php
+++ src/Infra/Persistence/Doctrine/Entity/UserEntity.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`users`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class UserEntity implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
