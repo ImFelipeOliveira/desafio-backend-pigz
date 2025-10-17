ff442f0043bfc7e990e964bef17df3b7a74acfe9
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-16 22:52:27 -0400

    fix: update bundle registration and adjust Doctrine configuration paths

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,15 @@
 <?php
 
-return [
+ $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
+
+// Register dev-only bundles only if they exist (prevents errors when --no-dev installs are used)
+if (class_exists(\Symfony\Bundle\MakerBundle\MakerBundle::class)) {
+    $bundles[\Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+return $bundles;
