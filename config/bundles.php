68f9c2e6138070b9d3999e04eb1bd5df5b925755
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-16 22:53:54 -0400

    fix: remove unnecessary comment about dev-only bundles in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -1,13 +1,12 @@
 <?php
 
- $bundles = [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
 
-// Register dev-only bundles only if they exist (prevents errors when --no-dev installs are used)
 if (class_exists(\Symfony\Bundle\MakerBundle\MakerBundle::class)) {
     $bundles[\Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
 }
